07261fbb971e43529e03ee561230b6bc08006236
Philip Anwa <ukpaiihechi@rocketmail.com>
2023-06-29 15:37:06 +0100

    add jScript files

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-document.querySelector("button").addEventListener("click", clickMe);
+/*var calBot = document.querySelectorAll(".bot").length;
 
-function clickMe() {
-    alert("I got Clicked!");
+for (var i = 0; i < calBot; i++) {
+
+    document.querySelectorAll(".bot")[i].addEventListener("click", clickMe);
+
+    function clickMe() {
+        alert("I got Clicked!");
+    }
+    
+}*/
+
+
+
+
+for (let index = 0; index < document.querySelectorAll(".bot").length; index++) {
+
+    document.querySelectorAll(".bot")[index].addEventListener("click", clickMe);
+
+    function clickMe() {
+        alert("I got Clicked!");
+    }
+    
 }
+
