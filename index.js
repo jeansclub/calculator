91ff69982a7146a72e8c033f5429087e4ebe1c97
Philip Anwa <ukpaiihechi@rocketmail.com>
2023-07-01 20:43:55 +0100

    edit css and javaScript

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ for (var i = 0; i < calBot; i++) {
 
 for (let index = 0; index < document.querySelectorAll(".bot").length; index++) {
 
-    document.querySelectorAll(".bot")[index].addEventListener("click", clickMe);
+    document.querySelectorAll(".bot")[index].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
 
-    function clickMe() {
-        alert("I got Clicked!");
-    }
+        alert(buttonInnerHTML + " got Clicked!");
+        buttonAnimation(buttonInnerHTML);
+    });
     
+}
+
+document.addEventListener("keypress", function(event) {
+
+    alert(event.key + " got Clicked!");
+    buttonAnimation(event.key);
+    
+});
+
+function buttonAnimation(currentKey) {
+    var pressedButton = document.querySelector("." + currentKey);
+    pressedButton.classList.add("pressed");
 }
 
