96e2ad2afda6dc1429b21a2f09baba753acb347b
Philip Anwa <ukpaiihechi@rocketmail.com>
2023-07-13 17:00:17 +0100

    modify javaScript file

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 for (let index = 0; index < document.querySelectorAll(".bot").length; index++) {
-    document.querySelectorAll(".bot")[index].addEventListener("click", function() {
-        
-        var buton = this;
-        buton.classList.add("shadow");
-
-        setTimeout(() => {
-           buton.classList.remove("shadow");
-        }, 250);
-        
-    });
     
+    document.querySelectorAll(".bot")[index].addEventListener("click", function() {
+  
+       var numb = this.innerHTML;
+
+       display(numb);
+ 
+    });  
 }
 
-document.addEventListener("keypress", function(event) {
-    var keyBoard = event.key;
-    alert(keyBoard + " was pressed");
-})
+
+document.querySelector(".equal").addEventListener("click", solve);
+
+document.querySelector(".clearscreen").addEventListener("click", cancelAll);
+
+
+
+
+function display(val) {
+    document.querySelector(".arith").value += val;
+    return val;
+}
+
+
+function solve() {
+
+    var x = document.querySelector(".arith").value;
+
+    var y = eval(x);
+
+    document.querySelector(".result").value = y;
+    return y;
+}
+
+function cancelAll() {
+    document.querySelector(".arith").value = "";
+    document.querySelector(".result").value = "";
+}
+
